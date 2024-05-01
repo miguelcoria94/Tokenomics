df809ed6cb2776cbcbabb272b9b86c00df0f3e3f
miguelcoria94 <miguelcoria94@gmail.com>
2024-05-01 17:26:09 -0500

    added coinmarketcap api

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+import requests
+import environ
 
-# Create your views here.
-
+env = environ.Env()
 def home(request):
-    return render(request, 'home.html')
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+    parameters = {
+        'start': '1',
+        'limit': '6',
+        'convert': 'USD'
+    }
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': env('COINMARKETCAP_API_KEY')
+    }
+
+    response = requests.get(url, headers=headers, params=parameters)
+    data = response.json()
+    
+    return render(request, 'home.html', {'cryptos': data['data']})
