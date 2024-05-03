25d02b861791a78d9a8058bb6502fcc1e0d75958
miguelcoria94 <miguelcoria94@gmail.com>
2024-05-02 21:10:58 -0500

    added coing and watchlist model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Cryptocurrency(models.Model):
+    crypto_id = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return f'Crypto ID: {self.crypto_id}'
+
+class Watchlist(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    cryptos = models.ManyToManyField(Cryptocurrency)
+
+    def __str__(self):
+        crypto_ids = ', '.join(str(crypto.crypto_id) for crypto in self.cryptos.all())
+        return f'{self.user.username} - {crypto_ids}'
